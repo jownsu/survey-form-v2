8741861246fd27293f975cd2083c56066b393690
Jownsu <jhonesdigno666@gmail.com>
2022-09-17 18:18:58 +0800

    haha

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -4,7 +4,7 @@ const chai                  = require('chai');
 const expect                = chai.expect;
 const SurveyModel           = require('../models/survey.model');
 
-//testing asd again
+//testing
 
 describe("Survey Model", function(){
 
